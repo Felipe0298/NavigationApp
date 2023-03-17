25d7950a3a16eb61949baeecd41e2fd131faf3d9
Felipe Hoyos <felipe02-98@hotmail.com>
2023-03-17 12:49:53 -0500

    106. Estilizando el Stack Navigator

--- src/screens/Pagina2Screen.tsx
+++ src/screens/Pagina2Screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, Text, View} from 'react-native';
 import { styles } from '../theme/appTheme';
 import { CommonActions } from '@react-navigation/native';
@@ -7,9 +7,20 @@ import { useNavigation } from '@react-navigation/core';
 export const Pagina2Screen = () => {
 
     const navigator = useNavigation();
+
+    useEffect(() => {
+      navigator.setOptions({
+        title:"Hola Mundo",
+        headerBackTitle:"" //Interpreta el nombre del boton de regreso dependiendo del idioma del telefono
+      })
+     
+    }, [])
+    
+    
+
     return (
         <View style={styles.globalMargin}>
-            <Text>Pagina2Screen</Text>
+            <Text style={styles.title}>Pagina2Screen</Text>
 
             <Button title='Ir a pagina 3' onPress={()=> navigator.dispatch(CommonActions.navigate({name:"Pagina3Screen"}))}/>
         </View>
